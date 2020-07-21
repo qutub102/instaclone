dacca49e8a7faee3218c069a8523c2134e8da1ce
Qutubuddin Bohra <qutubuddiniam@gmail.com>
2020-07-21 11:32:21 +0530

    Updates

---
+++ precache-manifest.2126ea2b2076cb0c860bbf3a6cc3c4a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa422fe5c7704f770bc7ba15f6602701",
+    "url": "/index.html"
+  },
+  {
+    "revision": "61f6e203220e6974fb28",
+    "url": "/static/css/main.5d9c644f.chunk.css"
+  },
+  {
+    "revision": "a3807831a412b3e141f8",
+    "url": "/static/js/2.d614e9f9.chunk.js"
+  },
+  {
+    "revision": "74c0ddca854ac003128884c6311253d0",
+    "url": "/static/js/2.d614e9f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61f6e203220e6974fb28",
+    "url": "/static/js/main.5f4d0e74.chunk.js"
+  },
+  {
+    "revision": "7a9bf887bf7cf5248395",
+    "url": "/static/js/runtime-main.2eefd23d.js"
+  }
+]);
